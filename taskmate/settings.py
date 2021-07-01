708aaa20fde312ccc62d7f54f027f6622fc0790d
Aggarwal Ankur <20006460@LTHE.IN>
2021-07-01 11:57:46 +0530

    project Updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -125,8 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [("static"),]
-STATIC_ROOT = os.path.join (BASE_DIR,'static_files')
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 CRISPY_TEMPLATE_PACK= 'bootstrap4'
 
 LOGIN_REDIRECT_URL = "todolist"
